a0715dfc8b63cf294544a331ef323546a71826b8
Samuel Carr <yorgishmorgies@gmail.com>
2021-03-26 21:02:30 -0400

    Fixed bounce pads

--- Assets/Scripts/BouncePad.cs
+++ Assets/Scripts/BouncePad.cs
@@ -8,44 +8,51 @@ public class BouncePad : MonoBehaviour
 
     public float bounceForce;
     public string direction;
+    public float disableTime;
 
     void OnCollisionEnter(Collision collision)
     {
         var rb = collision.rigidbody;
         rb.velocity = new Vector3(0, 0, 0);
 
+        // Reset jump count and dash
+        player.GetComponent<PlayerMovement>().currentJumps =
+            player.GetComponent<PlayerMovement>().maxJumps;
+
+        player.GetComponent<PlayerMovement>().hasDashed = false;
+
         switch (direction)
         {
             case "L":
-                player.GetComponent<PlayerMovement>().Jump(Vector3.left, bounceForce, false);
+                player.GetComponent<PlayerMovement>().PointLaunch(Vector3.left, bounceForce, disableTime);
                 // rb.AddForce(Vector3.left * bounceForce, ForceMode.Impulse);
                 break;
             case "R":
-                player.GetComponent<PlayerMovement>().Jump(Vector3.right, bounceForce, false);
+                player.GetComponent<PlayerMovement>().PointLaunch(Vector3.right, bounceForce, disableTime);
                 // rb.AddForce(Vector3.right * bounceForce, ForceMode.Impulse);
                 break;
             case "LU":
-                player.GetComponent<PlayerMovement>().Jump(new Vector3(-1, 1, 0), bounceForce, false);
+                player.GetComponent<PlayerMovement>().PointLaunch(new Vector3(-1, 1, 0), bounceForce, disableTime);
                 // rb.AddForce(new Vector3(-1, 1, 0) * bounceForce, ForceMode.Impulse);
                 break;
             case "RU":
-                player.GetComponent<PlayerMovement>().Jump(new Vector3(1, 1, 0), bounceForce, false);
+                player.GetComponent<PlayerMovement>().PointLaunch(new Vector3(1, 1, 0), bounceForce, disableTime);
                 // rb.AddForce(new Vector3(1, 1, 0) * bounceForce, ForceMode.Impulse);
                 break;
             case "D":
-                player.GetComponent<PlayerMovement>().Jump(Vector3.down, bounceForce, false);
+                player.GetComponent<PlayerMovement>().PointLaunch(Vector3.down, bounceForce, disableTime);
                 // rb.AddForce(Vector3.down * bounceForce, ForceMode.Impulse);
                 break;
             case "LD":
-                player.GetComponent<PlayerMovement>().Jump(new Vector3(-1, -1, 0), bounceForce, false);
+                player.GetComponent<PlayerMovement>().PointLaunch(new Vector3(-1, -1, 0), bounceForce, disableTime);
                 // rb.AddForce(new Vector3(-1, -1, 0) * bounceForce, ForceMode.Impulse);
                 break;
             case "RD":
-                player.GetComponent<PlayerMovement>().Jump(new Vector3(1, -1, 0), bounceForce, false);
+                player.GetComponent<PlayerMovement>().PointLaunch(new Vector3(1, -1, 0), bounceForce, disableTime);
                 // rb.AddForce(new Vector3(1, -1, 0) * bounceForce, ForceMode.Impulse);
                 break;
             default:
-                player.GetComponent<PlayerMovement>().Jump(Vector3.up, bounceForce, false);
+                player.GetComponent<PlayerMovement>().PointLaunch(Vector3.up, bounceForce, disableTime);
                 // rb.AddForce(Vector3.up * bounceForce, ForceMode.Impulse);
                 break;
         }
